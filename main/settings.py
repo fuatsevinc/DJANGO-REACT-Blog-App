7f4678bc177ac7c9c53e66e80e1ed191c144ee6a
fuatsevinc <fuatsevinc@icloude.com>
2022-06-26 13:46:11 +0200

    views.py added

--- main/settings.py
+++ main/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     
     # --- Kendi yazdigimiz APP ler ---
-    'blog',
+    #'blog',
 ]
 
 ROOT_URLCONF = 'main.urls'
